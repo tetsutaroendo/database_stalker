29d6a93129d1a6bc6e4e6d9dc114d5256a53007c
ShoichiroKitano <shoichiro.kitano@gaiax.com>
2017-10-30 14:09:10 +0900

    無駄なテストを削除

--- spec/database_stalker_spec.rb
+++ spec/database_stalker_spec.rb
@@ -4,8 +4,4 @@ describe DatabaseStalker do
   it "has a version number" do
     expect(DatabaseStalker::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
