ef08e55bc47aae46645f251d95db7d9455aa025d
ShoichiroKitano <shoichiro.kitano@gaiax.com>
2017-10-30 16:23:06 +0900

    正常終了の時のテスト

--- lib/database_stalker.rb
+++ lib/database_stalker.rb
@@ -1,5 +1,9 @@
 require "database_stalker/version"
 
 module DatabaseStalker
-  # Your code goes here...
+
+  def self.start(log_file, table_log_file)
+    File.open(table_log_file, 'w') do |f|
+    end
+  end
 end
--- spec/database_stalker_spec.rb
+++ spec/database_stalker_spec.rb
@@ -1,7 +1,39 @@
 require "spec_helper"
 
 describe DatabaseStalker do
-  it "has a version number" do
-    expect(DatabaseStalker::VERSION).not_to be nil
+  context 'version' do
+    it { expect(DatabaseStalker::VERSION).not_to be nil }
+  end
+
+  describe 'functions' do
+    before do
+      File.delete(test_log_path) if File.exists?(test_log_path)
+      File.delete(table_log_path) if File.exists?(table_log_path)
+    end
+    let(:test_log_path) { 'spec/fixture/test.log' }
+    let(:table_log_path) { 'spec/fixture/table.log' }
+
+    it do
+      described_class.start(test_log_path, table_log_path)
+      File.open(test_log_path, 'w') do |f|
+            log = <<-EOS
+    [1m[35mSQL (0.4ms)[0m  INSERT INTO `examples1` (`id`) VALUES (1)
+    [1m[35mSQL (0.4ms)[0m  INSERT INTO `examples2` (`name`) VALUES ('NAME')
+            EOS
+          f.puts log
+      end
+      result = nil
+      File.open(table_log_path, 'r') do |f|
+        f.each_line do |line|
+          result = line
+        end
+      end
+      expect(result).to be_nil
+    end
+
+    after do
+      File.delete(test_log_path) if File.exists?(test_log_path)
+      File.delete(table_log_path) if File.exists?(table_log_path)
+    end
   end
 end
