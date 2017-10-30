37832abe2e777232f31ca1fcecef7d967d56aade
ShoichiroKitano <shoichiro.kitano@gaiax.com>
2017-10-30 16:37:43 +0900

    正常終了時の挙動を追加

--- lib/database_stalker.rb
+++ lib/database_stalker.rb
@@ -3,7 +3,14 @@ require "database_stalker/version"
 module DatabaseStalker
 
   def self.start(log_file, table_log_file)
-    File.open(table_log_file, 'w') do |f|
+   main_pid = Process.pid
+    Process.fork do
+      thread = Process.detach(main_pid)
+      while true
+        break if thread.status == false
+      end
+      File.open(table_log_file, 'w') do |f|
+      end
     end
   end
 end
--- spec/database_stalker_spec.rb
+++ spec/database_stalker_spec.rb
@@ -1,5 +1,12 @@
 require "spec_helper"
 
+
+class ThreadStub
+  def status
+    false
+  end
+end
+
 describe DatabaseStalker do
   context 'version' do
     it { expect(DatabaseStalker::VERSION).not_to be nil }
@@ -7,33 +14,47 @@ describe DatabaseStalker do
 
   describe 'functions' do
     before do
-      File.delete(test_log_path) if File.exists?(test_log_path)
-      File.delete(table_log_path) if File.exists?(table_log_path)
+      clean_up_file(test_log_path)
+      clean_up_file(table_log_path)
     end
+
     let(:test_log_path) { 'spec/fixture/test.log' }
     let(:table_log_path) { 'spec/fixture/table.log' }
 
-    it do
-      described_class.start(test_log_path, table_log_path)
-      File.open(test_log_path, 'w') do |f|
-            log = <<-EOS
-    [1m[35mSQL (0.4ms)[0m  INSERT INTO `examples1` (`id`) VALUES (1)
-    [1m[35mSQL (0.4ms)[0m  INSERT INTO `examples2` (`name`) VALUES ('NAME')
-            EOS
-          f.puts log
-      end
-      result = nil
-      File.open(table_log_path, 'r') do |f|
-        f.each_line do |line|
-          result = line
+    context 'テストが正常終了' do
+      it do
+        allow(Process).to receive(:detach).and_return(ThreadStub.new)
+        described_class.start(test_log_path, table_log_path)
+        File.open(test_log_path, 'w') do |f|
+              log = <<-EOS
+  [1m[35mSQL (0.4ms)[0m  INSERT INTO `examples1` (`id`) VALUES (1)
+              EOS
+            f.puts log
         end
+        wait_for_process_lifecicle
+        result = nil
+        File.open(table_log_path, 'r') do |f|
+          f.each_line do |line|
+            result = line
+          end
+        end
+        expect(result).to be_nil
       end
-      expect(result).to be_nil
     end
 
     after do
-      File.delete(test_log_path) if File.exists?(test_log_path)
-      File.delete(table_log_path) if File.exists?(table_log_path)
+      clean_up_file(test_log_path)
+      clean_up_file(table_log_path)
     end
   end
+
+  private
+
+    def clean_up_file(file_path)
+      File.delete(file_path) if File.exists?(file_path)
+    end
+
+    def wait_for_process_lifecicle
+      sleep(2)
+    end
 end
