1d9c473420f847067c1f4da68046585d9a05771c
ShoichiroKitano <shoichiro.kitano@gaiax.com>
2017-11-06 18:27:42 +0900

    privateメソッドでリファクタ

--- lib/database_stalker.rb
+++ lib/database_stalker.rb
@@ -3,15 +3,28 @@ require "database_stalker/parser"
 
 module DatabaseStalker
 
-  def self.start(log_file, table_log_file)
-    Process.fork do
-      while true
-        break if Process.ppid == 1
+  class  << self
+
+    def start(log_file, table_log_file)
+      Process.fork do
+        watch_test_process
+        save_stalked_tables(log_file, table_log_file)
       end
+    end
+
+    def save_stalked_tables(log_file, table_log_file)
       File.open(table_log_file, 'w') do |f|
         parser = Parser.new(log_file)
         parser.table_names.each { |table| f.puts table }
       end
     end
+
+    def watch_test_process
+      while true
+        return if  Process.ppid == 1
+      end
+    end
   end
+
+  private_class_method :save_stalked_tables, :watch_test_process
 end
