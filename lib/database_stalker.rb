9cb68305da98463420eb141e2785dd69f6d6f16d
Tetsutaro Endo <tetsutaro.endo@gmail.com>
2017-11-06 16:48:43 +0900

    ログファイルからInsertテーブルをパース

--- lib/database_stalker.rb
+++ lib/database_stalker.rb
@@ -7,7 +7,14 @@ module DatabaseStalker
       while true
         break if Process.ppid == 1
       end
-      tables = ['examples1', 'examples2']
+
+      tables = []
+      File.open(log_file, 'r') do |f|
+        f.each_line do |line|
+          matched = line.match(/INSERT\ INTO\ `(.+)` \(/)
+          tables << matched[1] unless matched.nil?
+        end
+      end
       File.open(table_log_file, 'w') do |f|
         tables.each { |table| f.puts table }
       end
