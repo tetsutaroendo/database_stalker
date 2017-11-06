e39e5d8dc12de4b916ad7150d4dd0b89f4521370
Tetsutaro Endo <tetsutaro.endo@gmail.com>
2017-11-06 16:59:51 +0900

    テストログのフィクスチャ用意をプライベートメッソド化

--- spec/database_stalker_spec.rb
+++ spec/database_stalker_spec.rb
@@ -18,13 +18,11 @@ describe DatabaseStalker do
       it do
         allow(Process).to receive(:ppid).and_return(1)
         described_class.start(test_log_path, table_log_path)
-        File.open(test_log_path, 'w') do |f|
-              log = <<-EOS
+        log = <<-EOS
   [1m[35mSQL (0.4ms)[0m  INSERT INTO `examples1` (`id`) VALUES (1)
   [1m[35mSQL (0.4ms)[0m  INSERT INTO `examples2` (`id`) VALUES (1)
-              EOS
-            f.puts log
-        end
+        EOS
+        simulate_db_operation(test_log_path, log)
         wait_for_process_lifecicle
         expect(table_names_from_log(table_log_path)).to eq(['examples1', 'examples2'])
       end
@@ -37,6 +35,12 @@ describe DatabaseStalker do
   end
 
   private
+
+    def simulate_db_operation(log_path, log)
+      File.open(log_path, 'w') do |f|
+        f.puts log
+      end
+    end
 
     def table_names_from_log(log_path)
       result = []
