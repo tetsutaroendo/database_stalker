ae72a4f9dbb93e09b2d8fddacd7eb7540621ae0f
Tetsutaro Endo <tetsutaro.endo@gmail.com>
2017-11-08 16:42:53 +0900

    デーモンプロセスで代用

--- spec/database_stalker_spec.rb
+++ spec/database_stalker_spec.rb
@@ -27,17 +27,17 @@ describe DatabaseStalker do
     end
 
     it do
-      Process.fork do
+      Process.daemon do
         described_class.start(test_log_path, table_log_path)
         log = <<-EOS
-  [1m[35mSQL (0.4ms)[0m  INSERT INTO `examples1` (`id`) VALUES (1)
-  [1m[35mSQL (0.4ms)[0m  INSERT INTO `examples2` (`id`) VALUES (1)
+  [1m[35mSQL (0.4ms)[0m  INSERT INTO `examples` (`id`) VALUES (1)
         EOS
         simulate_db_operation(test_log_path, log)
-        exit(false) # simulate test process dies
+        #exit(false) # simulate test process dies
+        crash # simulate test process dies
       end
       sleep(2)
-      expect(table_names_from_log(table_log_path)).to eq(['examples1', 'examples2'])
+      expect(File.exists?(table_log_path)).to be_truthy
     end
 
     after do
