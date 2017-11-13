a606ac2d6f3c32471a6ad915c5116597af718921
ShoichiroKitano <shoichiro.kitano@gaiax.com>
2017-11-13 18:00:33 +0900

    railsと連携するテストを修正

--- spec/rails_spec.rb
+++ spec/rails_spec.rb
@@ -1,23 +1,25 @@
 require 'spec_helper'
 require 'open3'
+require 'bundler'
 
-xdescribe 'collaboratation with rails' do
-  before { clean_up_file(path_to_tables_log) }
+describe 'collaboratation with rails' do
+  before { clean_up_file(table_log_file) }
 
-  let(:current_directory) { Dir.pwd }
-  let(:path_to_tables_log) { 'rails_test/spec/tables.log' }
+  let(:table_log_file) { 'rails_test/log/table_names.log' }
 
   it do
     execute_test_of_rails_application
     sleep(2)
-    expect(File.exists?(path_to_tables_log)).to be_truthy
+    expect(DatabaseStalker.read_table_names(table_log_file: table_log_file)).to match_array(['sample1s', 'sample2s', 'sample3s'])
   end
 
-  after { clean_up_file(path_to_tables_log) }
+  after { clean_up_file(table_log_file) }
 
   private
 
     def execute_test_of_rails_application
-      Open3.capture3("cd rails_test && bundle exec rspec -I../lib spec/database_stalker_usage_spec.rb")
+      Bundler.with_clean_env do
+        Open3.capture3("cd rails_test && bundle exec rspec -I../lib spec/database_stalker_usage_spec.rb")
+      end
     end
 end
