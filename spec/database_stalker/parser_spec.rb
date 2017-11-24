fc876d0e93d3642f865ec09a62d68156b768427f
ShoichiroKitano <shoichiro.kitano@gaiax.com>
2017-11-24 14:42:28 +0900

    parserのインターフェースを変更

--- spec/database_stalker/parser_spec.rb
+++ spec/database_stalker/parser_spec.rb
@@ -2,22 +2,17 @@ require "spec_helper"
 require 'database_stalker/parser'
 
 describe DatabaseStalker::Parser do
-  let(:test_log_path) { 'spec/fixture/test.log' }
-
-  before { clean_up_file(test_log_path) }
-
   subject do
-    write_file(test_log_path, log)
-    parser = described_class.new(test_log_path)
+    parser = described_class.new(log)
     parser.table_names
   end
 
   context 'multiple inserted tables' do
     let(:log) do
-      <<-EOS
-  [1m[35mSQL (0.4ms)[0m  INSERT INTO `examples1` (`id`) VALUES (1)
-  [1m[35mSQL (0.4ms)[0m  INSERT INTO `examples2` (`id`) VALUES (1)
-      EOS
+      [
+        "  [1m[35mSQL (0.4ms)[0m  INSERT INTO `examples1` (`id`) VALUES (1)\n",
+        "  [1m[35mSQL (0.4ms)[0m  INSERT INTO `examples2` (`id`) VALUES (1)\n",
+      ]
     end
 
     it { is_expected.to eq(['examples1', 'examples2']) }
@@ -25,12 +20,12 @@ describe DatabaseStalker::Parser do
 
   context 'other log inclusion' do
     let(:log) do
-      <<-EOS
-  [1m[35m (0.2ms)[0m  BEGIN
-  [1m[36m (0.1ms)[0m  [1mSAVEPOINT active_record_1[0m
-  [1m[35mSQL (0.4ms)[0m  INSERT INTO `examples` (`id`) VALUES (1)
-  [1m[36m (0.1ms)[0m  [1mRELEASE SAVEPOINT active_record_1[0m
-      EOS
+      [
+        "  [1m[35m (0.2ms)[0m  BEGIN\n",
+        "  [1m[36m (0.1ms)[0m  [1mSAVEPOINT active_record_1[0m\n",
+        "  [1m[35mSQL (0.4ms)[0m  INSERT INTO `examples` (`id`) VALUES (1)\n",
+        "  [1m[36m (0.1ms)[0m  [1mRELEASE SAVEPOINT active_record_1[0m\n",
+      ]
     end
 
     it { is_expected.to eq(['examples']) }
@@ -38,14 +33,12 @@ describe DatabaseStalker::Parser do
 
   context 'table name duplication' do
     let(:log) do
-      <<-EOS
-  [1m[35mSQL (0.4ms)[0m  INSERT INTO `examples` (`id`) VALUES (1)
-  [1m[35mSQL (0.4ms)[0m  INSERT INTO `examples` (`id`) VALUES (2)
-      EOS
+      [
+        "  [1m[35mSQL (0.4ms)[0m  INSERT INTO `examples` (`id`) VALUES (1)\n",
+        "  [1m[35mSQL (0.4ms)[0m  INSERT INTO `examples` (`id`) VALUES (2)\n",
+      ]
     end
 
     it { is_expected.to eq(['examples']) }
   end
-
-  after { clean_up_file(test_log_path) }
 end
