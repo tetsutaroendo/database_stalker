9c6afcc2ade05040be794899d8106c9977d74a68
ShoichiroKitano <shoichiro.kitano@gaiax.com>
2017-11-27 15:44:55 +0900

    wip

--- spec/rails_spec.rb
+++ spec/rails_spec.rb
@@ -1,19 +1,21 @@
 require 'spec_helper'
 require 'open3'
 
-xdescribe 'collaboratation with rails' do
-  before { clean_up_file(path_to_tables_log) }
+describe 'collaboratation with rails' do
+  #before { clean_up_file(path_to_tables_log) }
 
   let(:current_directory) { Dir.pwd }
-  let(:path_to_tables_log) { 'rails_test/spec/tables.log' }
+  #let(:path_to_tables_log) { './rails_test/log/table_names.log' }
+  let(:path_to_tables_log) { '/Users/shoichirokitano/projects/database_stalker/rails_test/log/table_names.log' }
 
   it do
-    execute_test_of_rails_application
-    sleep(2)
     expect(File.exists?(path_to_tables_log)).to be_truthy
+    #execute_test_of_rails_application
+    #sleep(10)
+    #expect(File.exists?(path_to_tables_log)).to be_truthy
   end
 
-  after { clean_up_file(path_to_tables_log) }
+  #after { clean_up_file(path_to_tables_log) }
 
   private
 
